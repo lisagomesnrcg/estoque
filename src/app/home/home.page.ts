da96457c2c06789cd1c995af86c2e2cba414cb4d
lisagomesnrcg <naiaracgomes@gmail.com>
2023-04-14 21:06:11 -0300

    api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { ClientesService } from './../services/clientes.service';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -9,5 +10,13 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private clientesService: ClientesService) {
+    this.buscarClientes();
+  }
+
+  buscarClientes() {
+    this.clientesService.getAll().subscribe(dados => {
+      console.log(dados)
+    })
+  }
 }
