d646f8370b08783c2b7da5ddea3e8bbe6b7d5f7e
lisagomesnrcg <naiaracgomes@gmail.com>
2023-04-20 21:07:20 -0300

    kkkkkk

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,9 +1,50 @@
+import { Cliente } from './../models/Cliente.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  url = 'http://localhost:3000/clientes';
+
+  constructor(private http: HttpClient) { }
+
+  create(cliente: Cliente) {
+    return this.http.post(this.url, cliente);
+  }
+
+  getAll():Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.url).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirError(erro))
+    );
+  }
+
+  getOne(id: number) {
+    // this.http.get(this.url + '/' + id);
+    return this.http.get(`${this.url}/${id}`);
+  }
+
+  update(cliente: Cliente) {
+    return this.http.put(`${this.url}/${cliente.id}`, cliente);
+  } 
+
+  delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+
+  login() { }
+
+  logout() { }
+
+  exibirError(erro: any): Observable<any>{
+
+    alert('Deu erro!')
+    console.log(erro)
+    return EMPTY;
+
+  }
 }
